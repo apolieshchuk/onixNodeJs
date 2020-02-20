544fd63b84e1e4f5ba119826d7a267fc7f27bdc4
apolieshchuk <anton2013ua@gmail.com>
2020-02-20 11:47:24 +0200

    fix(events.js): fix error listener parameters (according valerii checha typo-fix commit)

--- src/server/events.js
+++ src/server/events.js
@@ -44,8 +44,8 @@ function onListening() {
  * @inner
  * @param {http.Server} Server
  */
-function bind(Server) {
-  Server.on('error', (error) => this.onError(error, Server.get('port')));
+function bind(Server, port) {
+  Server.on('error', (error) => this.onError(error, port));
   Server.on('listening', this.onListening.bind(Server));
 }
 
